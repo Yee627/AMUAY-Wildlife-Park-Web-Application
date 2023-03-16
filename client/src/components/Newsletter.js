8d23d3b84e28f0ac25ab62ce20f8ca27089381cb
Yee627 <zoeyee0627@gmail.com>
2023-03-16 12:02:23 +0000

    update css

--- client/src/components/Newsletter.js
+++ client/src/components/Newsletter.js
@@ -33,7 +33,7 @@ function Newsletter() {
   return (
     <div className="newsletter">
       <h2>GET THE NEWSLETTER</h2>
-      <div className="eyebrow">Sign up for wildlife news, zoo activities and more</div>
+      <div className="comehere">Sign up for wildlife news, zoo activities and more</div>
       <form onSubmit={handleSubmit} >
         <input onChange={handleChange} type="text" id="email" value={email} name="email" placeholder="Email Address" />
         <input type="submit" className="subscribe" value="SUBMIT" />
